ecbcc2114c3d2215234fc7e7c54b6a2b5684d850
NishinoTsukasa <35717703+nishinosukasa@users.noreply.github.com>
2018-10-30 23:52:38 +0700

    Add files via upload

---
+++ Source.py
@@ -0,0 +1,46 @@
+## Source Code
+# TakeScript Deface In Sites 
+# Import Folder Script In Android
+# mv Codename.html /storage/emulated/0/ 
+# Check Folder Script TakeScript In Memory Internal Name Files Codename.html
+# Special Thanks To :* TheChoyon
+import urllib
+import os
+import sys
+import time
+import random
+
+#Clear
+os.system ('clear')
+#Wallpaper
+print ('''
+\033[91m
+  _______                                         _______              __         
+ |   _   | .-----. .--.--. .----. .----. .-----. |   _   | .-----. .--|  | .-----.
+ |   1___| |  _  | |  |  | |   _| |  __| |  -__| |.  1___| |  _  | |  _  | |  -__|
+ |____   | |_____| |_____| |__|   |____| |_____| |.  |___  |_____| |_____| |_____|
+ |:  1   |                                       |:  1   |                        
+ |::.. . |                                       |::.. . |                        
+ `-------'                                       `-------'                      
+                                                                                
+                   \033[92m[\033[91m127.0.0.1\033[92m]\033[0m|_|\033[92m[\033[91m127.217.21.78\033[92m]
+                       \033[92m[\033[91mCodec By Codename\033[92m]
+''')
+target = raw_input('http://: ')
+try:
+    bukaurl = urllib.urlopen(target)
+    baca = openurl.read()
+except:
+    print '\033[91mCheck Connection Internet. With Protocol http dont https'
+    input("press enter to exit ...")
+print '\033[92mSource Code Successful Save...'
+file_save = open('codename.html', 'w')
+file_save.write(baca)
+print '\033[93mSource Code Save as codename.html'
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.3)
+mengetik('^_^ Done TakeScript...')
+key = raw_input("ENTER To exit")
